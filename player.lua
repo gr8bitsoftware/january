0262380791ed2f65e8d89b8a9b40faeda56f8d30
Taylor O'Brien <obrien.taylorm@gmail.com>
2015-01-08 21:13:46 -0600

    Put players on top of buildings.

--- player.lua
+++ player.lua
@@ -2,8 +2,20 @@ require "constants"
 
 Player = {}
 
+local function alignPlayer(building)
+  local buildingCenter = (2 * building.x + building.width) / 2
+  return buildingCenter - (PLAYER_WIDTH / 2)
+end
+
 Player.generate = function(map, color)
-  return {x = math.random(0, map.width), y = math.random(0, map.height), color = color}
+  local playerX = math.random(0, map.width)
+  for _, building in ipairs(map.buildings) do
+    if playerX > building.x and playerX < building.x + building.width then
+      return {x = alignPlayer(building), y = building.y - PLAYER_HEIGHT, color = color}
+    end
+  end
+
+  return nil
 end
 
 Player.draw = function(p)
